2866fd74906b994c5f11d5d45b0b2debbf8f7f3a
Eytan <58141479+Eytan12@users.noreply.github.com>
2019-12-09 20:02:48 +0200

    Update form.php

--- form.php
+++ form.php
@@ -1,18 +1,5 @@
 
 <?php
-
-$dbServername = "localhost";
-$dbUsername = "u815710449_playtolearn";
-$dbPassword = "Sce2019";
-$dbName="u815710449_playtolearn";
-$conn = new mysqli($dbServername, $dbUsername, $dbPassword, $dbName);
-$name = $_GET["username"];
-$sex = $_GET["sexe"];
-$email = $_GET["email"];
-$age = $_GET["age"];
-$contry = $_GET["pays"];
-$password = password_hash($_GET["password"],PASSWORD_DEFAULT);
-
 $dbServername = "localhost"; //Connect to server
 $dbUsername = "u815710449_playtolearn"; // Username Database
 $dbPassword = "Sce2019"; // Password Database
@@ -30,7 +17,6 @@ if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
     $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
 } else {
     $ip = $_SERVER['REMOTE_ADDR'];
-}
 }//Collect Adress IP 
 // Check connection
 if ($conn->connect_error) {
