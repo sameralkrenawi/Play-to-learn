bb079df1e90f46f171ab1daf68551003f7069f3d
Eytan <58141479+Eytan12@users.noreply.github.com>
2019-12-09 11:12:30 +0200

    Update form.php

--- form.php
+++ form.php
@@ -7,21 +7,24 @@ $dbPassword = "Sce2019";
 $dbName="u815710449_playtolearn";
 $conn = new mysqli($dbServername, $dbUsername, $dbPassword, $dbName);
 $name = $_GET["username"];
-$password = $_GET["password"];
 $sex = $_GET["sexe"];
 $email = $_GET["email"];
 $age = $_GET["age"];
 $contry = $_GET["pays"];
+$password = password_hash($_GET["password"],PASSWORD_DEFAULT);
+
+if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+    $ip = $_SERVER['HTTP_CLIENT_IP'];
+} elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+    $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+} else {
+    $ip = $_SERVER['REMOTE_ADDR'];
+}
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-
-$sql = "INSERT INTO MyGuests (username,email,sex,password,pays,age)
-VALUES ('$name','$email','$sex','$password','$contry','$age')";
+$sql = "INSERT INTO MyGuests (username,email,sex,password,pays,age,Adress_IP)
+VALUES ('$name','$email','$sex','$password','$contry','$age','$ip')";
 $conn->query($sql);
-
-
-//$result = $conn->query($sql);
-
 ?>
