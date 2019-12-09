266800896436dbe0e2d94e6375a426cfc0f56194
Eytan12 <58141479+Eytan12@users.noreply.github.com>
2019-12-09 12:12:50 +0200

    Add files via upload

--- form.php
+++ form.php
@@ -1,28 +1,27 @@
 
 <?php
+
 $dbServername = "localhost";
 $dbUsername = "u815710449_playtolearn";
 $dbPassword = "Sce2019";
 $dbName="u815710449_playtolearn";
 $conn = new mysqli($dbServername, $dbUsername, $dbPassword, $dbName);
 $name = $_GET["username"];
+$password = $_GET["password"];
 $sex = $_GET["sexe"];
 $email = $_GET["email"];
-$age = $_GET["country"];
-$country = $_GET["pays"];
-$password=password_hash($_GET["password"],PASSWORD_DEFAULT);
-if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
-    $ip = $_SERVER['HTTP_CLIENT_IP'];
-} elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
-    $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
-} else {
-    $ip = $_SERVER['REMOTE_ADDR'];
-}
+$age = $_GET["age"];
+$contry = $_GET["pays"];
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-$sql = "INSERT INTO MyGuests (username,email,sex,password,pays,age,Adress_IP)
-VALUES ('$name','$email','$sex','$password','$country','$age','$ip')";
+
+$sql = "INSERT INTO MyGuests (username,email,sex,password,pays,age)
+VALUES ('$name','$email','$sex','$password','$contry','$age')";
 $conn->query($sql);
+
+
+//$result = $conn->query($sql);
+
 ?>
