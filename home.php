ba94d3c53cbe3afe455e264cb93e7fcf67d87beb
Eytan <58141479+Eytan12@users.noreply.github.com>
2019-12-16 21:20:21 +0200

    Create home.php

---
+++ home.php
@@ -0,0 +1,20 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <!-- importer le fichier de style -->
+        <link rel="stylesheet" href="style.css" media="screen" type="text/css" />
+    </head>
+    <body style='background:#fff;'>
+        <div id="content">
+            <?php
+                session_start();
+                if($_SESSION['username'] !== ""){
+                    $user = $_SESSION['username'];
+                    // afficher un message
+                    echo "Bonjour $user, vous êtes connecté";
+                }
+            ?>
+            
+        </div>
+    </body>
+</html>
