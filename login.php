7af7d8636d5e8e0b004f53ae4e4bb5c5114509db
Eytan <58141479+Eytan12@users.noreply.github.com>
2019-12-16 21:14:42 +0200

    Create login.php

---
+++ login.php
@@ -0,0 +1,30 @@
+?php 
+session_start();
+$dbServername = "localhost";
+$dbUsername = "u815710449_playtolearn";
+$dbPassword = "Sce2019";
+$dbName="u815710449_playtolearn";
+$db = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName)
+           or die('could not connect to database');
+$username = mysqli_real_escape_string($db,htmlspecialchars($_POST['username'])); 
+$password = mysqli_real_escape_string($db,htmlspecialchars($_POST['password']));
+$requete = "SELECT * FROM MyGuests";
+$exec_requete = mysqli_query($db,$requete);
+$repons = mysqli_fetch_array($exec_requete);
+$temp=0;
+echo $temp;
+while($repons = mysqli_fetch_array($exec_requete) and $temp=1){
+    if($repons['username']==$username and (password_verify($password,$repons['password'])==1)){
+        session_start();
+        $_SESSION['username'] = $username;
+        echo 'Vous êtes connecté !';
+        echo $repons['username'];
+        $temp=1;
+        header('Location:/Sign-up.html');
+        exit();
+    }
+}
+echo $temp;
+    header('Location:/Sign-in-error.html');
+   exit();
+?>
