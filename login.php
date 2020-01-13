adf7d63e721389f7a17acbcbd561c1b608dc5baf
Eytan <58141479+Eytan12@users.noreply.github.com>
2020-01-13 19:11:21 +0200

    Update login.php

--- login.php
+++ login.php
@@ -20,11 +20,27 @@ while($repons = mysqli_fetch_array($exec_requete) and $temp=1){
         echo 'Vous êtes connecté !';
         echo $repons['username'];
         $temp=1;
+        header('Location:/home1.php');
+        exit();
+    }
+}
+$username = mysqli_real_escape_string($db,htmlspecialchars($_POST['username'])); 
+$password = mysqli_real_escape_string($db,htmlspecialchars($_POST['password']));
+$requete = "SELECT * FROM MyParents";
+$exec_requete = mysqli_query($db,$requete);
+$repons = mysqli_fetch_array($exec_requete);
+$temp=0;
+while($repons = mysqli_fetch_array($exec_requete) and $temp=1){
+    if($repons['username']==$username and (password_verify($password,$repons['password'])==1)){
+        session_start();
+        $_SESSION['username'] = $username;
+        echo 'Vous êtes connecté !';
+        echo $repons['username'];
+        $temp=1;
         header('Location:/home.php');
         exit();
     }
 }
-echo $temp;
     header('Location:/Sign-in-error.html');
    exit();
 ?>
