ec9bbdb7c4541f596e2c8a4c29681dfe67754fe5
sameralkrenawi <samer-335@hotmail.co.il>
2020-01-13 20:26:57 +0200

    all
    all

---
+++ ForgotPassword.php
@@ -0,0 +1,70 @@
+<?php 
+$dbServername = "localhost";
+$dbUsername = "u815710449_playtolearn";
+$dbPassword = "Sce2019";
+$dbName="u815710449_playtolearn";
+$db = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName)
+           or die('could not connect to database');
+$username = mysqli_real_escape_string($db,htmlspecialchars($_POST['username'])); 
+$requete = "SELECT * FROM MyGuests";
+$exec_requete = mysqli_query($db,$requete);
+$repons = mysqli_fetch_array($exec_requete);
+$temp=0;
+while($repons = mysqli_fetch_array($exec_requete) and $temp=1){
+    if($repons['email']==$username){
+        ini_set( 'display_errors', 1 );
+        error_reporting( E_ALL );
+        $new_pass = rand();
+        $new_pass_crypt=password_hash($new_pass,PASSWORD_DEFAULT);
+        $sql = "UPDATE MyGuests SET password='$new_pass_crypt' WHERE email = '$username'";
+        $resultat = mysqli_query($db,$sql);
+        if ($resultat == FALSE) { 
+            echo "Echec de l'exécution de la requête.<br />"; 
+        } 
+        $from = "contact@playtolearn.website";
+        $to = $repons['email'];
+         $header="MIME-Version: 1.0\r\n";
+        $header.='From:"PlayToLearn"<password@playtolearn.website>'."\n";
+        $header.='Content-Type:text/html; charset="utf-8"'."\n";
+        $header.='Content-Transfer-Encoding: 8bit';
+        $subject = "Your new password";
+        $message='
+         <html>
+         <head>
+           <title>Your password</title>
+           <meta charset="utf-8" />
+         </head>
+         <body>
+           <font color="#303030";>
+             <div align="center">
+               <table width="600px">
+                 <tr>
+                   <td>
+                     
+                     <div align="center">Bonjour <b>'.$repons['username'].'</b>,</div>
+                     This is your new password <b>'.$new_pass.'</b>
+                     See you soon on PlayToLearn <a href="#">playtolearn.website</a> !
+                     
+                   </td>
+                 </tr>
+                 <tr>
+                   <td align="center">
+                     <font size="2">
+                       This is an automatic mail. 
+                     </font>
+                   </td>
+                 </tr>
+               </table>
+             </div>
+           </font>
+         </body>
+         </html>
+         ';
+        mail($to,$subject,$message, $header);
+        header('Location:/Sign-in-NewPassword.html');
+        exit();
+    }
+}
+
+    header('Location:/ForgotPassword-Error.html');
+?>
