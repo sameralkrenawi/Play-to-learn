51a967c172cf3aa1413fe1a603ef4cd7586c4b62
Eytan <58141479+Eytan12@users.noreply.github.com>
2020-01-17 06:29:11 +0200

    Update yourChildaccount.php

--- yourChildaccount.php
+++ yourChildaccount.php
@@ -1,4 +1,5 @@
 <?php session_start();?>
+
 <!DOCTYPE html>
 <html>
     <style>
@@ -19,39 +20,54 @@
         <meta charset="utf-8">
 		<link rel="stylesheet" href="css/Sign-in.css">
 	</head>
-
 	<body> 
 	<div class='login-form'>
-		<form>
-			<div class="imgcontainer">
-				<img src="image/imageLogin.png" alt="Avatar" class="avatar"> 
+	    <form>
+	        <div class="imgcontainer">
+				          <?php 
+		if(!empty($_SESSION['image'])){    
+		echo "<img src='image/".$_SESSION['image']."' style=\"
+        width: 150px;
+        border-radius: 50%;
+        display: block;
+        margin-left: auto;
+        margin-right: auto;
+        height: 150px;
+        \">";}
+        else echo "<img src=\"image/imageLogin.png\" alt=\"Avatar\" class=\"avatar\"> ";
+        ?> 
 			</div>
+	
+		    <div id="background1"></div>
 
-			<div id="background1"></div>
-
-			<div class="container">
-				<form class="form-signin"> 
+			<div class="container" style="padding-top: 20px;">
+				<form class="form-signin">    
+				
 					<?php
                     if (empty($_SESSION['username']) or $_SESSION['base']!="Child")
                     {
+                        
                     echo '<center><font color="red" size="4"><b>You need to be connected </center></font><br />';
                     echo("<center onclick=\"window.location.href = 'Sign-in.html';\"class=\"button\">Login</center>");
                     exit();
                     }
                     if($_SESSION['username'] !== ""){
+                        if(empty($_SESSION['image']))
+                            echo "<a class=\"button\" onclick=\"window.location.href = 'Newpicture.php';\" type=\"submit\" name=\"upload\">Choose Picture</a>";
+                        else echo "<a class=\"button\" onclick=\"window.location.href = 'Newpicture.php';\" type=\"submit\" name=\"upload\">Change Picture</a>";
+                        echo '<br>';
+                        echo '<br>';
                     echo '<a style="text-decoration:underline;" >Your Username: </a> <br>';
                     echo $_SESSION['username'].'<br>';
                        echo '<a style="text-decoration:underline;" >Your Email: </a> <br>';
                     echo $_SESSION['email'].'<br>';
                        echo '<a style="text-decoration:underline;" >Your last Connection: </a> <br>';
                     echo $_SESSION['last_connection'];
+                    
                 }
             ?>  
 				</form>
+			
 			</div>
 			<button onclick="window.location.href = 'youraccountChild.php';" class="button">Return</button>
 	</div>
-	
-		</form>
-	</body>
-</html>
